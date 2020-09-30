e083be6f7d859c2ba8f71da3e4d0f079dfffc36c
Jorge Winder <jorge.winder@gmail.com>
2020-09-30 11:31:58 -0500

    últimos cambios

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,20 @@
 const $btn = document.querySelector('#btnDescubrir')
 
 $btn.addEventListener('click', function(e) {
-    
+
     document.querySelector('.paso2').classList.add('out-panel')
     document.querySelector('.paso3').classList.add('in-panel')
+
+    document.querySelector('#mascara1').classList.add('escala-mascara')
+
+    setTimeout(() => {  document.querySelector('#mascara2').classList.add('escala-mascara'); }, 2000);
+    setTimeout(() => {  document.querySelector('#mascara3').classList.add('escala-mascara'); }, 3000);
+    setTimeout(() => {  document.querySelector('#mascara4').classList.add('escala-mascara'); }, 4000);
+    
+})
+
+
+document.querySelector('#mascara1').addEventListener('click', function(e) {
+
 
 })
