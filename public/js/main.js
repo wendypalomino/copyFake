6d75a54d84a75ad8b16bfd981addaf6558b5adfd
Jorge Winder <jorge.winder@gmail.com>
2020-10-06 22:49:41 -0500

    Ultimos cambios

--- public/js/main.js
+++ public/js/main.js
@@ -14,8 +14,41 @@ $btn.addEventListener('click', function(e) {
 })
 
 
-document.querySelector('#mascara1').addEventListener('click', function(e) {
+document.querySelector('#btnCamino').addEventListener('click', function(e) {
 
-    document.querySelector('.msg').classList.add('capa-msg')
+    document.querySelector('.paso3').classList.remove('in-panel')
+    document.querySelector('.paso3').classList.add('out-panel')
+    document.querySelector('.paso4').classList.add('in-panel')
 
 })
+
+
+document.querySelector('#mascara1').addEventListener('click', function(e) {
+    document.querySelector('.msg').classList.remove("capa-msg-off")
+    document.querySelector('.msg').classList.add('capa-msg')
+})
+
+document.querySelector('#mascara2').addEventListener('click', function(e) {
+    document.querySelector('.msg').classList.remove("capa-msg-off")
+    document.querySelector('.msg').classList.add('capa-msg')
+})
+
+document.querySelector('#mascara3').addEventListener('click', function(e) {
+    document.querySelector('.msg').classList.remove("capa-msg-off")
+    document.querySelector('.msg').classList.add('capa-msg')
+})
+
+document.querySelector('#mascara4').addEventListener('click', function(e) {
+    document.querySelector('.msg').classList.remove("capa-msg-off")
+    document.querySelector('.msg').classList.add('capa-msg')
+})
+
+
+document.querySelector('.cerrar').addEventListener('click', function(e) {
+
+    document.querySelector('.msg').classList.add('capa-msg-off')
+    setTimeout(() => {  document.querySelector('.msg').classList.remove('capa-msg'); }, 800);
+    
+
+})
+
