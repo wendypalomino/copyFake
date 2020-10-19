1afc493244bc696d31702539567e74c2843beaa4
Jorge Winder <jorge.winder@gmail.com>
2020-10-19 02:44:48 -0500

    Ultimos cambios

--- public/js/main.js
+++ public/js/main.js
@@ -1,54 +1,15 @@
-const $btn = document.querySelector('#btnDescubrir')
+const $page1 = document.querySelector('.page1')
+const $page2 = document.querySelector('.page2')
 
-$btn.addEventListener('click', function(e) {
+setTimeout(() => {  $page1.classList.add('out-panel'); }, 5000);
 
-    document.querySelector('.paso2').classList.add('out-panel')
-    document.querySelector('.paso3').classList.add('in-panel')
-
-    document.querySelector('#mascara1').classList.add('escala-mascara')
-
-    setTimeout(() => {  document.querySelector('#mascara2').classList.add('escala-mascara'); }, 2000);
-    setTimeout(() => {  document.querySelector('#mascara3').classList.add('escala-mascara'); }, 3000);
-    setTimeout(() => {  document.querySelector('#mascara4').classList.add('escala-mascara'); }, 4000);
+$page1.addEventListener("animationend", (e)=>{
+    
+    if(e.animationName === "outPanel"){
+        
+        // $page2.style.animationName = "inPanel"
+        // $page2.style.animationDuration = "1s"
+        $page2.classList.add('in-panel')
+    }
     
 })
-
-
-document.querySelector('#btnCamino').addEventListener('click', function(e) {
-
-    document.querySelector('.paso3').classList.remove('in-panel')
-    document.querySelector('.paso3').classList.add('out-panel')
-    document.querySelector('.paso4').classList.add('in-panel')
-
-})
-
-
-document.querySelector('#mascara1').addEventListener('click', function(e) {
-    document.querySelector('.msg').classList.remove("capa-msg-off")
-    document.querySelector('.msg').classList.add('capa-msg')
-})
-
-document.querySelector('#mascara2').addEventListener('click', function(e) {
-    document.querySelector('.msg').classList.remove("capa-msg-off")
-    document.querySelector('.msg').classList.add('capa-msg')
-})
-
-document.querySelector('#mascara3').addEventListener('click', function(e) {
-    document.querySelector('.msg').classList.remove("capa-msg-off")
-    document.querySelector('.msg').classList.add('capa-msg')
-})
-
-document.querySelector('#mascara4').addEventListener('click', function(e) {
-    document.querySelector('.msg').classList.remove("capa-msg-off")
-    document.querySelector('.msg').classList.add('capa-msg')
-})
-
-
-document.querySelector('.cerrar').addEventListener('click', function(e) {
-
-    document.querySelector('.msg').classList.add('capa-msg-off')
-    setTimeout(() => {  document.querySelector('.msg').classList.remove('capa-msg'); }, 800);
-    
-
-})
-
