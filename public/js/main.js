61b6add779f79560c9861e068b72b224d8fc920d
Jorge Winder <jorge.winder@gmail.com>
2020-10-20 05:36:48 -0500

    Ultimos cambios

--- public/js/main.js
+++ public/js/main.js
@@ -6,10 +6,9 @@ setTimeout(() => {  $page1.classList.add('out-panel'); }, 5000);
 $page1.addEventListener("animationend", (e)=>{
     
     if(e.animationName === "outPanel"){
-        
         // $page2.style.animationName = "inPanel"
         // $page2.style.animationDuration = "1s"
-        $page2.classList.add('in-panel')
+        $page.classList.add('in-panel')
     }
     
 })
