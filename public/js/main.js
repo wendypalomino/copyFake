6b67b78c001f89a6dfb93e771189e2fd3e09f56e
Jorge Winder <jorge.winder@gmail.com>
2020-12-03 15:46:34 -0500

    Ultimos cambios

--- public/js/main.js
+++ public/js/main.js
@@ -122,84 +122,66 @@ document.querySelector('.page6').addEventListener("animationend", (e)=>{
 // Animacion para pagina 6 y 7
 
 document.querySelector('.page6').addEventListener("wheel", (e)=>{
-
     ejecutaAnimacion('.page7After', 'in-panel', 1)
-    // ejecutaAnimacion('.page7', 'in-panel', 2)
-
-    // setTimeout(() => {  document.querySelector('.page7').classList.replace('in-panel', 'efecto-miedo'); }, 4000);
-    
-    ejecutaAnimacion('.page7', 'miedo', 2)
-    
-
+    ejecutaAnimacion('.page7', 'in-panel', 2)
 })
 
 
-// document.querySelector('.page6 .text-small').addEventListener("animationend", (e)=>{
-
-//     if(e.animationName === "inPanel"){
-
-//         setTimeout(() => {  document.querySelector('.page7').classList.add('in-panel'); }, 2000);
-//         setTimeout(() => {  document.querySelector('.page7 img').classList.add('in-panel'); }, 3000);
-
-//         setTimeout(() => {  document.querySelector('.page7 .container-text').classList.add('bottom-panel') }, 4000);
-//         setTimeout(() => {  document.querySelector('.page7 .boca').classList.add('in-panel'); }, 6000);
-
-
-//         // setTimeout(() => {  document.querySelector('.page8').classList.add('in-panel'); }, 2000);
-        
-//     }
-
-// })
+document.querySelector('.page7').addEventListener("animationend", (e)=>{
+    
+    if(e.animationName === "inPanel"){
+        ejecutaAnimacion(".page7 img", "scale-back-to-front", 1)
+        ejecutaAnimacion('.page7 .container-text', 'bottom-to-top', 3)
+        ejecutaAnimacion('.page7 .container-text div', 'transform-left-to-right-small', 6)
+        ejecutaAnimacion('.page7 .boca', 'scale-front-to-back', 8)
+    }
+})
 
 // Animacion para pagina 8
 
-// document.querySelector('.page7').addEventListener("wheel", (e)=>{
-
-//     setTimeout(() => {  document.querySelector('.page8').classList.add('in-panel'); }, 1000);
-
-// })
-
-document.querySelector('.page8').addEventListener("animationend", (e)=>{
-
-    if(e.animationName === "inPanel"){
-
-        setTimeout(() => {  document.querySelector('.page9').classList.add('in-panel'); }, 1000);
-        setTimeout(() => {  document.querySelector('.page9 img').classList.add('in-panel'); }, 2000);
-        setTimeout(() => {  document.querySelector('.page9 .text-small').classList.add('in-panel'); }, 3000);
-
-    }
-
+document.querySelector('.page7').addEventListener("wheel", (e)=>{
+    ejecutaAnimacion('.page8', 'in-panel', 1)
+    ejecutaAnimacion('.page9', 'in-panel', 2)
 })
 
 // Animacion para pagina 9
 
-document.querySelector('.page9').addEventListener("wheel", (e)=>{
+document.querySelector('.page9').addEventListener("animationend", (e)=>{
 
-    setTimeout(() => {  document.querySelector('.page10').classList.add('bottom-panel'); }, 1000);
+    if(e.animationName === "inPanel"){
 
-})
-
-// Animacion para pagina 10
-
-document.querySelector('.page10').addEventListener("animationend", (e)=>{
-
-    if(e.animationName === "bottomPanel"){
-
-        setTimeout(() => {  document.querySelector('.page11').classList.add('in-panel'); }, 1000);
-        setTimeout(() => {  document.querySelector('.page11 img').classList.add('in-panel'); }, 3000);
-
-        setTimeout(() => {  document.querySelector('.page11 .container-text').classList.add('bottom-panel') }, 4000);
-        setTimeout(() => {  document.querySelector('.page11 .boca').classList.add('in-panel'); }, 6000);
+        ejecutaAnimacion('.page9  img', 'in-panel', 2)
+        ejecutaAnimacion('.page9 .text-small-medium', 'in-panel', 3)
 
     }
 
 })
+
+
+// Animacion para pagina 10
+
+document.querySelector('.page9').addEventListener("wheel", (e)=>{
+
+    ejecutaAnimacion('.page10', 'bottom-panel', 1)
+
+    ejecutaAnimacion('.page11After', 'in-panel', 3)
+    ejecutaAnimacion('.page11', 'in-panel', 5)
+    ejecutaAnimacion('.page11 img', 'scale-back-to-front', 7)
+    ejecutaAnimacion('.page11 .container-text', 'bottom-to-top', 9)
+    ejecutaAnimacion('.page11 .container-text div', 'transform-left-to-right-big', 11)
+    ejecutaAnimacion('.page11 .boca', 'scale-front-to-back', 13)
+
+
+
+})
+
 
 // Animacion para pagina 11 / 12 / 13
 
 document.querySelector('.page11').addEventListener("wheel", (e)=>{
 
-    setTimeout(() => {  document.querySelector('.page12').classList.add('in-panel'); }, 1000);
+    ejecutaAnimacion('.page12', 'in-panel', 1)
+
 
 })
 
@@ -208,32 +190,46 @@ document.querySelector('.page12').addEventListener("animationend", (e)=>{
 
     if(e.animationName === "inPanel"){
 
-        setTimeout(() => {  document.querySelector('.page13').classList.add('in-panel'); }, 1000);
-        setTimeout(() => {  document.querySelector('.page13 .text-big-ultra').classList.add('in-panel'); }, 2000);
-        setTimeout(() => {  document.querySelector('.page13 p').classList.add('in-panel'); }, 3000);
+        ejecutaAnimacion('.page13', 'in-panel', 1)
+
+        ejecutaAnimacion('.page13 .mcaida1', 'caida', 2)
+        ejecutaAnimacion('.page13 .mcaida2', 'caida', 3)
+        ejecutaAnimacion('.page13 .mcaida3', 'caida', 4)
+
+        ejecutaAnimacion('.page13 .text-big-ultra', 'bottom-to-top', 6)
+        ejecutaAnimacion('.page13 .container-text', 'in-panel', 7)
+        ejecutaAnimacion('.page13 .barra-r', 'transform-left-to-right-xsmall', 8)
 
     }
 
 })
 
+
+
 // Animacion para pagina 14 / 15  / 16
 
 document.querySelector('.page13').addEventListener("wheel", (e)=>{
 
-    setTimeout(() => {  document.querySelector('.page13 p').classList.replace('in-panel','out-panel'); }, 1000);
-    setTimeout(() => {  document.querySelector('.page13 .text-big-ultra').classList.replace('in-panel','out-panel'); }, 2000);
+    // setTimeout(() => {  document.querySelector('.page13 p').classList.replace('in-panel','out-panel'); }, 1000);
+    // setTimeout(() => {  document.querySelector('.page13 .text-big-ultra').classList.replace('in-panel','out-panel'); }, 2000);
 
-    setTimeout(() => {  document.querySelector('.page14').classList.add('in-panel'); }, 3000);
+    ejecutaAnimacion('.page14After', 'bottom-panel', 2)
 
 })
 
 
-document.querySelector('.page14').addEventListener("animationend", (e)=>{
+document.querySelector('.page14After').addEventListener("animationend", (e)=>{
 
-    if(e.animationName === "inPanel"){
+    if(e.animationName === "bottomPanel"){
 
-        setTimeout(() => {  document.querySelector('.page15').classList.add('right-panel'); }, 1000);
-        setTimeout(() => {  document.querySelector('.page16').classList.add('in-panel'); }, 3000);
+        ejecutaAnimacion('.page14', 'in-panel', 2)
+        ejecutaAnimacion('.page15', 'right-panel-opacity', 4)
+
+        // ejecutaAnimacion('.page16', 'in-panel', 6.1)
+        ejecutaAnimacion('.page16', 'right-panel-opacity', 6)
+
+        // setTimeout(() => {  document.querySelector('.page15').classList.add('right-panel'); }, 1000);
+        // setTimeout(() => {  document.querySelector('.page16').classList.add('in-panel'); }, 3000);
 
     }
 
@@ -246,10 +242,9 @@ document.querySelector('.page16').addEventListener("animationend", (e)=>{
 
         setTimeout(() => {  document.querySelector('.page16 .text-big-ultra').classList.add('in-panel'); }, 2000);
         setTimeout(() => {  document.querySelectorAll('.page16 .text-small')[0].classList.add('in-panel'); }, 3000);
-        setTimeout(() => {  document.querySelectorAll('.page16 p')[0].classList.add('in-panel'); }, 4000);
-        setTimeout(() => {  document.querySelectorAll('.page16 p')[1].classList.add('in-panel'); }, 5000);
+        setTimeout(() => {  document.querySelector('.page16 .pr1').classList.add('in-panel'); }, 4000);
+        setTimeout(() => {  document.querySelector('.page16 .pr2').classList.add('in-panel'); }, 4000);
         setTimeout(() => {  document.querySelectorAll('.page16 .text-small')[1].classList.add('in-panel'); }, 6000);
-
 
     }
 
@@ -257,36 +252,38 @@ document.querySelector('.page16').addEventListener("animationend", (e)=>{
 
 document.querySelector('.page16').addEventListener("wheel", (e)=>{
     
-
-    setTimeout(() => {  document.querySelector('.page17').classList.add('in-panel'); }, 1000);
+    ejecutaAnimacion('.page17', 'in-panel', 1)
 
 })
 
 document.querySelector('.page17').addEventListener("animationend", (e)=>{
 
     if(e.animationName === "inPanel"){
-        setTimeout(() => {  document.querySelector('.page17 .text-big-ultra').classList.add('in-panel'); }, 1000);
+        ejecutaAnimacion('.page17 .text-big-ultra', 'in-panel', 1)
+
     }
 
 })
 
 document.querySelector('.page17').addEventListener("wheel", (e)=>{
-    
-    setTimeout(() => {  document.querySelector('.page18').classList.add('bottom-panel'); }, 1000);
+
+    ejecutaAnimacion('.page18', 'bottom-panel', 1)
+    ejecutaAnimacion('.page18 #gop19', 'in-panel', 3)
 
 })
 
 
 document.querySelector('#gop19').addEventListener("click", function(e) {
 
-    document.querySelector('.page19').classList.add('in-panel');
+    // document.querySelector('.page19').classList.add('in-panel');
+    ejecutaAnimacion('.page19', 'in-panel', 2)
 
 })
 
 
 document.querySelector('#gop20').addEventListener("click", function(e) {
 
-    document.querySelector('.page20').classList.add('in-panel');
+    ejecutaAnimacion('.page20', 'in-panel', 1)
 
 })
 
@@ -306,7 +303,7 @@ document.querySelector('.page21').addEventListener("animationend", (e)=>{
         setTimeout(() => {  document.querySelector('.page21 img').classList.add('in-panel'); }, 1000);
         setTimeout(() => {  document.querySelector('.page21 .text-big').classList.add('in-panel'); }, 2000);
         setTimeout(() => {  document.querySelector('.page21 .text-small').classList.add('in-panel'); }, 3000);
-        setTimeout(() => {  document.querySelector('.page22').classList.add('in-panel'); }, 7000);
+        // setTimeout(() => {  document.querySelector('.page22').classList.add('in-panel'); }, 7000);
     }
 
 })
